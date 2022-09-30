2fbc1a75e812ccda11352ebd99e0db05b5f2f5ee
Lillian Brevik <77015337+lillian-se@users.noreply.github.com>
2022-09-30 08:36:12 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,6 +9,8 @@ const UART_TX_CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
 // Allows a connected client to send a byte array
 const UART_RX_CHARACTERISTIC_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
 
+screen.orientation.lock("landscape");
+
 let uBitDevice;
 let rxCharacteristic;
 
